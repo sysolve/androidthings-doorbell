178dfef7c0d003e53b9e07641e4b6e7e101e05e0
Ray <ray@sysolve.com>
2018-01-22 20:28:36 +0800

    first commit

---
+++ app/src/main/java/com/sysolve/androidthings/doorbell/MainActivity.java
@@ -0,0 +1,175 @@
+package com.sysolve.androidthings.doorbell;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManagerService;
+import com.sysolve.androidthings.utils.BoardSpec;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private DoorbellCamera mCamera;
+
+    /**
+     * A {@link Handler} for running Camera tasks in the background.
+     */
+    private Handler mCameraHandler;
+    private Handler handler = new Handler();
+
+    Gpio mButtonGpio;
+
+    /**
+     * An additional thread for running Camera tasks that shouldn't block the UI.
+     */
+    private HandlerThread mCameraThread;
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // We need permission to access the camera
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // A problem occurred auto-granting the permission
+            Log.d(TAG, "No permission");
+            return;
+        }
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        DoorbellCamera.dumpFormatInfo(this);
+
+        // Creates new handlers and associated threads for camera and networking operations.
+        mCameraThread = new HandlerThread("CameraBackground");
+        mCameraThread.start();
+        mCameraHandler = new Handler(mCameraThread.getLooper());
+
+        // Camera code is complicated, so we've shoved it all in this closet class for you.
+        mCamera = DoorbellCamera.getInstance();
+        mCamera.initializeCamera(this, mCameraHandler, mOnImageAvailableListener);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCamera.takePicture();
+            }
+        });
+
+        PeripheralManagerService service = new PeripheralManagerService();
+
+        try {
+            mButtonGpio = service.openGpio(BoardSpec.getGoogleSampleButtonGpioPin());
+            mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+            mButtonGpio.registerGpioCallback(new GpioCallback() {
+                @Override
+                public boolean onGpioEdge(Gpio gpio) {
+                    buttonPressedTimes++;
+                    Log.i(TAG, "GPIO changed, button pressed " + buttonPressedTimes);
+
+                    mCamera.takePicture();
+
+                    // Return true to continue listening to events
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    long buttonPressedTimes = 0;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCamera.shutDown();
+
+        mCameraThread.quitSafely();
+
+        if (mButtonGpio!=null) try {
+            mButtonGpio.close();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Listener for new camera images.
+     */
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener =
+            new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = reader.acquireLatestImage();
+                    // get image bytes
+                    ByteBuffer imageBuf = image.getPlanes()[0].getBuffer();
+                    final byte[] imageBytes = new byte[imageBuf.remaining()];
+                    imageBuf.get(imageBytes);
+                    image.close();
+
+                    onPictureTaken(imageBytes);
+                }
+            };
+
+    /**
+     * Handle image processing in Firebase and Cloud Vision.
+     */
+    private void onPictureTaken(final byte[] imageBytes) {
+        if (imageBytes != null) {
+            String imageStr = Base64.encodeToString(imageBytes, Base64.NO_WRAP | Base64.URL_SAFE);
+            Log.d(TAG, "imageBase64:"+imageStr);
+
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            if (bitmap != null) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        }
+    }
+}
